5c2b3c3dc5dd410d7433ef989d01a1f19eb992d5
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-02-17 09:27:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react;'
+
+
+
 
 function App() {
-  return <h1>Hi Hubert</h1>
+  return (<h1>Hi Hubert</h1>
+          );
 }
 
 export default App;
